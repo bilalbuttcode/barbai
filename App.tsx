7de9f26c33bc827a4303464d279cc28c3beba764
bilalbuttcode <bilalbutthere@gmail.com>
2025-08-04 12:03:18 +0500

    Today

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react'
+import Login from './src/Screens/AuthScreens/Login';
+import Signup from './src/Screens/AuthScreens/Signup';
 
 const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
